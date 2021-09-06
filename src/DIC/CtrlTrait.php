7cd1afa608fd16e3be70d61701b15871d6e94d06
Adrian Lüthi <adi.l@bluewin.ch>
2021-09-06 16:24:26 +0200

    initual ctrl trait

---
+++ src/DIC/CtrlTrait.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types = 1);
+
+namespace Fluxlabs\Assessment\Tools\DIC;
+
+use ilCtrl;
+
+/**
+ * trait CtrlTrait
+ *
+ * @package Fluxlabs\Assessment\Tools
+ *
+ * @author Fluxlabs AG <adi@fluxlabs.ch>
+ */
+trait CtrlTrait
+{
+    private ilCtrl $ctrl;
+
+    private function getCtrl() : ilCtrl
+    {
+        if ($this->ctrl === null) {
+            global $DIC;
+            $this->ctrl = $DIC->ctrl();
+        }
+
+        return $this->ctrl;
+    }
+
+    public function getCommandLink(string $command) : string
+    {
+        return $this->getCtrl()->getLinkTargetByClass(
+            $this->getCtrl()->getCmdClass(),
+            $command
+        );
+    }
+}
